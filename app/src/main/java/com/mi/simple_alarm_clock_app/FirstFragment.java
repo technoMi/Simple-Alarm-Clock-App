e86117036c2890fe1ddf316afb89bf6e9d961cad
Mi <nulevoigradus95@gmail.com>
2025-02-27 14:58:20 +0600

    Some changes

--- app/src/main/java/com/mi/simple_alarm_clock_app/FirstFragment.java
+++ app/src/main/java/com/mi/simple_alarm_clock_app/FirstFragment.java
@@ -2,13 +2,26 @@ package com.mi.simple_alarm_clock_app;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.core.view.MenuHost;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.mi.simple_alarm_clock_app.databinding.FragmentFirstBinding;
+
 public class FirstFragment extends Fragment {
+
+    private FragmentFirstBinding binding;
+
+    private NavController navController;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,7 +31,27 @@ public class FirstFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+
+        binding = FragmentFirstBinding.inflate(inflater, container, false);
+
+        navController = Navigation.findNavController(requireActivity(), R.id.fragmentContainerView);
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View View, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(View, savedInstanceState);
+
+        setHasOptionsMenu(true);
+
+        binding.btnSettings.setOnClickListener(v -> {
+            navController.navigate(R.id.action_firstFragment_to_secondFragment);
+        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
     }
 }
