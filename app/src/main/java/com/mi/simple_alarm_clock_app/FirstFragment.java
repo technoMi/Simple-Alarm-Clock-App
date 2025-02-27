6e6c8975835038fe906cab614d79712d29da9b00
Michail Baranov <nulevoigradus90>
2025-02-27 20:55:32 +0600

    Navigation in main fragment done

--- app/src/main/java/com/mi/simple_alarm_clock_app/FirstFragment.java
+++ app/src/main/java/com/mi/simple_alarm_clock_app/FirstFragment.java
@@ -4,7 +4,9 @@ import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.MenuHost;
+import androidx.core.view.MenuProvider;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -12,21 +14,18 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.material.appbar.MaterialToolbar;
 import com.mi.simple_alarm_clock_app.databinding.FragmentFirstBinding;
 
-public class FirstFragment extends Fragment {
+public class FirstFragment extends Fragment implements MenuProvider {
 
     private FragmentFirstBinding binding;
 
     private NavController navController;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -40,18 +39,31 @@ public class FirstFragment extends Fragment {
     }
 
     @Override
-    public void onViewCreated(@NonNull View View, @Nullable Bundle savedInstanceState) {
-        super.onViewCreated(View, savedInstanceState);
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        setHasOptionsMenu(true);
 
-        binding.btnSettings.setOnClickListener(v -> {
-            navController.navigate(R.id.action_firstFragment_to_secondFragment);
-        });
+        MaterialToolbar toolbar = view.findViewById(R.id.mtToolbar);
+        ((AppCompatActivity) requireActivity()).setSupportActionBar(toolbar);
+
+        requireActivity().addMenuProvider(this, getViewLifecycleOwner());
     }
 
     @Override
-    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
-        super.onCreateOptionsMenu(menu, inflater);
+    public void onCreateMenu(@NonNull Menu menu, @NonNull MenuInflater menuInflater) {
+        menuInflater.inflate(R.menu.menu, menu);
+    }
+
+    @Override
+    public boolean onMenuItemSelected(@NonNull MenuItem menuItem) {
+        int itemId = menuItem.getItemId();
+        if (itemId == R.id.miSettings) {
+            navigate(R.id.action_firstFragment_to_secondFragment);
+        }
+        return true;
+    }
+
+    private void navigate(int destination) {
+        navController.navigate(destination);
     }
 }
